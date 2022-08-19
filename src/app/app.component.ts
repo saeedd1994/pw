e26ab030dfc5abee68aa5af20d4d81e5e021a9c4
saeed <saeed.da1994@gmail.com>
2022-08-19 21:59:23 +0430

    first state of implementation without style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pw';
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+  pass: string = '';
+  lengthPass: number = 0;
+  validChar: string = '';
+  passGenerated: string = '';
+
+  onChangeUseLetters(): void {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(): void {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(): void {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(e: any) {
+    const parseVal = parseInt(e.target.value);
+    if (!isNaN(parseVal)) {
+      this.lengthPass = parseVal;
+    }
+  }
+
+  onBtnClicked(): void {
+    const validLetters = 'abcdefghijklmnopqrstuvwxyz';
+    const validNumbers = '1234567890';
+    const validSymbols = '!@#$%^&*()_+';
+    if (this.includeLetters) {
+      this.validChar += validLetters;
+    }
+    if (this.includeNumbers) {
+      this.validChar += validNumbers;
+    }
+    if (this.includeSymbols) {
+      this.validChar += validSymbols;
+    }
+
+    for (let i = 0; i < this.lengthPass; i++) {
+      const index = Math.floor(Math.random() * this.validChar.length)
+      this.passGenerated += this.validChar[index];
+    }
+
+    this.pass = this.passGenerated;
+  }
 }
